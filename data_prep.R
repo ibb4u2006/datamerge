c30cd58e616deb4edbffadb9508177fdfa369bcf
D352 <D352@czu.cz>
2018-11-02 11:17:26 +0100

    regression part1

--- data_prep.R
+++ data_prep.R
@@ -65,10 +65,85 @@ annual_means_cor <- cor(annual_means_tab[, -1], use = "complete.obs")
 ann_m_m <- melt(annual_means_cor)
 
 ggplot(ann_m_m) +
-  geom_tile(aes(x = Var1, y = Var2, fill = value)) +
-  geom_text(aes(x = Var1, y = Var2, label = round(value, 2)), col = 'white') +
+  geom_tile(aes(x = Var1, y = Var2, fill = values)) +
+  geom_text(aes(x = Var1, y = Var2, label = round(values, 2)), col = 'white') +
   coord_fixed() +
   theme_bw()
 
 library(corrplot)
 library(RColorBrewer)
+
+
+my_col <- rev(colorRampPalette(brewer.pal(11, "RdBu"))(100))
+corrplot(annual_means_cor, col = my_col)
+
+corrplot(annual_means_cor, 
+         type = "upper", 
+         tl.col = "black", 
+         tl.pos = "d", 
+         col = my_col)
+corrplot(annual_means_cor, 
+         add = TRUE, 
+         type = "lower", 
+         method = "number", 
+         cl.lim = c(-1,1), 
+         col = my_col, 
+         diag = FALSE,
+         tl.pos = "n", 
+         cl.pos = "n")
+
+
+
+
+monthly_means_tab <- dcast(monthly_means, year ~ variable + station)
+monthly_means_cor <- cor(monthly_means_tab[, -1], use = "complete.obs")
+
+month_m_m <- melt(monthly_means_cor)
+
+ggplot(month_m_m) +
+  geom_tile(aes(x = Var1, y = Var2, fill = values)) +
+  geom_text(aes(x = Var1, y = Var2, label = round(values, 2)), col = 'white') +
+  coord_fixed() +
+  theme_bw()
+
+library(corrplot)
+library(RColorBrewer)
+
+
+my_col <- rev(colorRampPalette(brewer.pal(11, "RdBu"))(100))
+corrplot(monthly_means_cor, col = my_col)
+
+corrplot(monthly_means_cor, 
+         type = "upper", 
+         tl.col = "black", 
+         tl.pos = "d", 
+         col = my_col)
+corrplot(monthly_means_cor, 
+         add = TRUE, 
+         type = "lower", 
+         method = "number", 
+         cl.lim = c(-1,1), 
+         col = my_col, 
+         diag = FALSE,
+         tl.pos = "n", 
+         cl.pos = "n")
+
+
+#Regression
+oslo_temp_annual <- annual_means_tab[, c('year', 'temp_oslo')]
+ggplot(data = oslo_temp_annual, aes(x = year, y = temp_oslo)) +
+  geom_line() + 
+  geom_smooth(method = 'lm', se = F) + 
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  theme_bw()
+
+fitted_model <- lm(temp_oslo ~ year, data = oslo_temp_annual)
+
+resid <- data.table(year = oslo_temp_annual$year[as.numeric(names(residuals(fitted_model)))])
+resid$residuals <- residuals(fitted_model)
+
+ggplot(data = resid, aes(x = year, y = residuals)) +
+  geom_point() + 
+  geom_smooth(se = F) + 
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  theme_bw()
